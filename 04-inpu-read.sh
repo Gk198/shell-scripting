16c734c2ff339540aa2d4c5693472856b37b1a5b
Gk198 <gorakh.kumar@gmail.com>
2021-09-04 10:51:20 +0000

    some more script

---
+++ 04-inpu-read.sh
@@ -0,0 +1,6 @@
+#!bin/bash
+
+
+read -p "Enter your Name: " name
+
+echo "your Name = ${name}"
---
+++ 05-input-spacial-var.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+
+echo script name = $0
+echo first agrument = $1
+echo second agrument = $2
+
+echo all agrument = $*
+
+echo all agrument = $@
+
+echo Number of agrument = $#
