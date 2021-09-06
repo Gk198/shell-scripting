3efabdefec2793b7f209a1f51e781dcc3a6a9717
Gk198 <gorakh.kumar@gmail.com>
2021-09-06 09:41:03 +0000

    MongoDB Script

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -1,18 +1,6 @@
 #!bin/bash 
 
-Status_Check() {
-  if [ $1 -eq 0 ]; then 
-    echo -e "\e[32mCOMPLETED\e[0m"
-  else 
-    echo -e "\e[31mNOTCOMPLETED\e[0m"
-    exit 2
-  fi 
-}
-
-Print() {
-  echo -e "\n\t\t\e[36m----------------- $1 ----------------------\e[0m\n" &>>/tmp/log
-  echo -n -e "$1 \t- "
-}
+source components/common.sh
 
 Print "Setting up Mongodb repo"
 
