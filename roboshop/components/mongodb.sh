c02bf28d6c0e9021c2bcd977289d86da13c83309
Gk198 <gorakh.kumar@gmail.com>
2021-09-06 08:52:04 +0000

    MongoDB script

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -8,8 +8,10 @@ else
    exit 2
 fi
 }
-
-echo "Setting up Mongodb repo"
+Print() {
+echo -n -e "$1"
+}
+Print "Setting up Mongodb repo"
 
 echo '[mongodb-org-4.2]
 name=MongoDB Repository
@@ -17,39 +19,40 @@ baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
 gpgcheck=1
 enabled=1
 gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
-status_check $?
+status_checks $?
 
-  echo "Installing Mongodb"
+  Print "Installing Mongodb"
 
  yum install -y mongodb-org &>>/tmp/log
  
- status_check $?
+ status_checks $?
  
-   echo "Configuring Mongodb"
+   Print "Configuring Mongodb"
  
  sed -i -e 's/127.0.0.0/0.0.0.0/' /etc/mongod.conf
  status_check $?
 
  
-   echo "Starting Mongodb"
+   Print "Starting Mongodb"
  systemctl enable mongod
  systemctl restart mongod
  status_check $?
 
 
-  echo "Downloading Mongodb schema"
+  Print "Downloading Mongodb schema"
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
 
 status_check $?
 
  cd /tmp
-    echo "Extracting mongodb Archive"
+    Print "Extracting mongodb Archive"
  unzip -o mongodb.zip &>>/tmp/log
-status_check $?
+ status_check $?
 
  cd mongodb-main
-   echo "Loading schema"
+   Print "Loading schema"
  mongo < catalogue.js &>>/tmp/log
  mongo < users.js  &>>/tmp/log
-status_check $?
+ status_check $?
  
+ exit 0
