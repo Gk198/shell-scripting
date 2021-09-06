b7a810e5b8049f939409206935cb85631319ee98
Gk198 <gorakh.kumar@gmail.com>
2021-09-06 09:03:44 +0000

    MongoDB

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -8,6 +8,7 @@ else
    exit 2
 fi
 }
+
 Print() {
 echo -n -e "$1"
 }
@@ -19,32 +20,26 @@ baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
 gpgcheck=1
 enabled=1
 gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
-status_check $?
+ status_check $?
 
-  Print "Installing Mongodb"
-
- yum install -y mongodb-org &>>/tmp/log
- 
+Print "Installing Mongodb"
+  yum install -y mongodb-org &>>/tmp/log
  status_check $?
  
-   Print "Configuring Mongodb"
- 
+ Print "Configuring Mongodb"
  sed -i -e 's/127.0.0.0/0.0.0.0/' /etc/mongod.conf
  status_check $?
 
- 
-   Print "Starting Mongodb"
+Print "Starting Mongodb"
  systemctl enable mongod
  systemctl restart mongod
  status_check $?
 
-
-  Print "Downloading Mongodb schema"
+Print "Downloading Mongodb schema"
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
-
 status_check $?
 
- cd /tmp
+cd /tmp
     Print "Extracting mongodb Archive"
  unzip -o mongodb.zip &>>/tmp/log
  status_check $?
