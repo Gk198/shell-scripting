c0754dafcf06cd6f9b9b7f528d019b5ffa59d5a3
Gk198 <gorakh.kumar@gmail.com>
2021-09-06 07:53:32 +0000

    monogodb

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -9,13 +9,30 @@ gpgcheck=1
 enabled=1
 gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
 
+if [ $? -eq 0 ]; then
+   echo -e "\e[32mCOMPLETED\e[0m"
+else 
+   echo -e "\e[31mNOTCOMPLETED\e[0m"
+fi
+
+   
 echo "Installing Mongodb"
 
  yum install -y mongodb-org &>>/tmp/log
+ if [ $? -eq 0 ]; then
+   echo -e "\e[32mCOMPLETED\e[0m"
+else 
+   echo -e "\e[31mNOTCOMPLETED\e[0m"
+fi
  
  echo "Configuring Mongodb"
  
  sed -i -e 's/127.0.0.0/0.0.0.0/' /etc/mongod.conf
+ if [ $? -eq 0 ]; then
+   echo -e "\e[32mCOMPLETED\e[0m"
+else 
+   echo -e "\e[31mNOTCOMPLETED\e[0m"
+fi
  
 echo "Starting Mongodb"
  systemctl enable mongod
@@ -28,9 +45,19 @@ curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongo
  cd /tmp
  echo "Extracting mongodb Archive"
  unzip -o mongodb.zip &>>/tmp/log
+ if [ $? -eq 0 ]; then
+   echo -e "\e[32mCOMPLETED\e[0m"
+else 
+   echo -e "\e[31mNOTCOMPLETED\e[0m"
+fi
  cd mongodb-main
  echo "Loading schema"
  mongo < catalogue.js &>>/tmp/log
  mongo < users.js  &>>/tmp/log
+ if [ $? -eq 0 ]; then
+   echo -e "\e[32mCOMPLETED\e[0m"
+else 
+   echo -e "\e[31mNOTCOMPLETED\e[0m"
+fi
  
  
