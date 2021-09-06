592939151dd2898feeecc84f967d9d066cb381e6
Gk198 <gorakh.kumar@gmail.com>
2021-09-06 09:15:41 +0000

    MongoDB Script

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -22,34 +22,34 @@ baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
 gpgcheck=1
 enabled=1
 gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
- status_check $?
+Status_Check $?
 
 Print "Installing Mongodb"
   yum install -y mongodb-org &>>/tmp/log
- status_check $?
+ Status_Check $?
  
  Print "Configuring Mongodb"
  sed -i -e 's/127.0.0.0/0.0.0.0/' /etc/mongod.conf
- status_check $?
+ Status_Check $?
 
 Print "Starting Mongodb"
  systemctl enable mongod
  systemctl restart mongod
- status_check $?
+ Status_Check $?
 
 Print "Downloading Mongodb schema"
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
-status_check $?
+Status_Check $?
 
 cd /tmp
     Print "Extracting mongodb Archive"
  unzip -o mongodb.zip &>>/tmp/log
- status_check $?
+ Status_Check $?
 
  cd mongodb-main
    Print "Loading schema"
  mongo < catalogue.js &>>/tmp/log
  mongo < users.js  &>>/tmp/log
- status_check $?
+ Status_Check $?
  
  exit 0
