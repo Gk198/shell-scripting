7056dd2a0918e1dfe3a2202f8bc811e56820b046
Gk198 <gorakh.kumar@gmail.com>
2021-09-06 08:39:36 +0000

    MongoDB script

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -1,5 +1,14 @@
 #!bin/bash 
 
+Status_checks() {
+ if [ $1 -eq 0 ]; then
+   echo -e "\e[32mCOMPLETED\e[0m"
+else 
+   echo -e "\e[31mNOTCOMPLETED\e[0m"
+   exit 2
+fi
+}
+
 echo "Setting up Mongodb repo"
 
 echo '[mongodb-org-4.2]
@@ -8,75 +17,39 @@ baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
 gpgcheck=1
 enabled=1
 gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
+status_check $?
 
-
-if [ $? -eq 0 ]; then
-   echo -e "\e[32mCOMPLETED\e[0m"
-else 
-   echo -e "\e[31mNOTCOMPLETED\e[0m"
-   exit 2
-fi
-
-   
-echo "Installing Mongodb"
+  echo "Installing Mongodb"
 
  yum install -y mongodb-org &>>/tmp/log
- if [ $? -eq 0 ]; then
-   echo -e "\e[32mCOMPLETED\e[0m"
-else 
-   echo -e "\e[31mNOTCOMPLETED\e[0m"
-   exit 2
-fi
  
- echo "Configuring Mongodb"
+ status_check $?
+ 
+   echo "Configuring Mongodb"
  
  sed -i -e 's/127.0.0.0/0.0.0.0/' /etc/mongod.conf
- if [ $? -eq 0 ]; then
-   echo -e "\e[32mCOMPLETED\e[0m"
-else 
-   echo -e "\e[31mNOTCOMPLETED\e[0m"
-   exit 2
-fi
+ status_check $?
+
  
    echo "Starting Mongodb"
  systemctl enable mongod
  systemctl restart mongod
- if [ $? -eq 0 ]; then
-   echo -e "\e[32mCOMPLETED\e[0m"
-else 
-   echo -e "\e[31mNOTCOMPLETED\e[0m"
-   exit 2
-fi
+ status_check $?
 
 
   echo "Downloading Mongodb schema"
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
 
-if [ $? -eq 0 ]; then
-   echo -e "\e[32mCOMPLETED\e[0m"
-else 
-   echo -e "\e[31mNOTCOMPLETED\e[0m"
-   exit 2
-fi
+status_check $?
 
  cd /tmp
     echo "Extracting mongodb Archive"
  unzip -o mongodb.zip &>>/tmp/log
- if [ $? -eq 0 ]; then
-   echo -e "\e[32mCOMPLETED\e[0m"
-else 
-   echo -e "\e[31mNOTCOMPLETED\e[0m"
-   exit 2
-fi
+status_check $?
+
  cd mongodb-main
    echo "Loading schema"
  mongo < catalogue.js &>>/tmp/log
  mongo < users.js  &>>/tmp/log
- if [ $? -eq 0 ]; then
-   echo -e "\e[32mCOMPLETED\e[0m"
-else 
-   echo -e "\e[31mNOTCOMPLETED\e[0m"
-   exit 2
-fi
- 
+status_check $?
  
