4aa0e241b148c3b5d9bdb5494544436ec246486a
Gk198 <gorakh.kumar@gmail.com>
2021-09-07 13:52:03 +0000

    catalogue script1

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -26,7 +26,7 @@ Status_Check $?
 
 Print "Extracting Catalogue"
  cd /home/roboshop
- unzip /tmp/catalogue.zip &>>/tmp/log
+ unzip -o /tmp/catalogue.zip &>>/tmp/log
  mv catalogue-main catalogue &>>/tmp/log
 Status_Check $?
 
