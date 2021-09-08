59d5febe9c0eace73ba1688cf62b32b62eb270d2
Gk198 <gorakh.kumar@gmail.com>
2021-09-08 08:25:31 +0000

    catalogue script

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -35,7 +35,7 @@ Print "Download NodeJS Dependancy"
  chown roboshop:roboshop -R /home/roboshop
  
  Print "update systemd service"
- sed -i -e 's/MONGO_DNSNAME:/mongodb.roboshop.internal/' /home/roboshop/catalogue/systemd.service
+ sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/roboshop/catalogue/systemd.service
  Status_Check $?
  
  Print "Setup systemd service\t"
