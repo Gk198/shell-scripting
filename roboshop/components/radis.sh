1959fe2b3fe0fa6dc76268cfd0f0ca7db2d02042
Gk198 <gorakh.kumar@gmail.com>
2021-09-08 09:17:55 +0000

    radis script

--- roboshop/components/radis.sh
+++ roboshop/components/radis.sh
@@ -10,19 +10,19 @@ Print "Install Yum Utils & Download Radis Repos"
  yum install epel-release yum-utils  yum install http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y &>>/tmp/log
  Status_Check $?
  
- Print "Setup Radis Repos"
+ Print "Setup Radis Repos\t\t\t"
  yum-config-manager --enable remi &>>/tmp/log
 Status_Check $?
 
 
-Print "Install Radis"
+Print "Install Radis\t\t\t"
 yum install redis -y &>>/tmp/log
 Status_Check $?
 
-Print "Configure redis listen Address"
+Print "Configure redis listen Address\t"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf 
 Status_Check $?
 
-Print "Start Redis service"
+Print "Start Redis service\t\t\"
 systemctl enable redis &>>/tmp/log && systemctl start redis &>>/tmp/log
 Status_Check $?
