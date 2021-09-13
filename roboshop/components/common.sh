cef41a4a1183e29045eb7a06e47682825d1dfe8c
Gk198 <gorakh.kumar@gmail.com>
2021-09-13 13:39:12 +0000

    common update

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -19,4 +19,48 @@ if [ $SUI -ne 0 ]; then
     
     fi
     
-    
+    ADD_APP_USER() {
+  Print "Adding RoboShop User\t"
+  id roboshop &>>$LOG
+  if [ $? -eq 0 ]; then
+    echo "User already there, So Skipping" &>>$LOG
+  else
+    useradd roboshop &>>$LOG
+  fi
+  Status_Check $?
+}
+
+DOWNLOAD() {
+  Print "Downloading ${COMPONENT} Content"
+  curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/roboshop-devops-project/${COMPONENT}/archive/main.zip" &>>$LOG
+  Status_Check $?
+  Print "Extracting ${COMPONENT}\t"
+  cd /home/roboshop
+  rm -rf ${COMPONENT} && unzip -o /tmp/${COMPONENT}.zip &>>$LOG && mv ${COMPONENT}-main ${COMPONENT}
+  Status_Check $?
+}
+
+SystemdD_Setup() {
+  Print "Update SystemD Service\t"
+  sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e  's/MONGO_ENDPOINT/mongodb.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' -e 's/CARTENDPOINT/cart.roboshop.internal/' -e 's/DBHOST/mysql.roboshop.internal/' -e 's/CARTHOST/cart.roboshop.internal/' -e 's/USERHOST/user.roboshop.internal/' -e 's/AMQPHOST/rabbitmq.roboshop.internal/'  /home/roboshop/${COMPONENT}/systemd.service
+  Status_Check $?
+
+  Print "Setup SystemD Service\t"
+  mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service && systemctl daemon-reload && systemctl restart ${COMPONENT} &>>$LOG && systemctl enable ${COMPONENT} &>>$LOG
+  Status_Check $?
+}
+
+NODEJS() {
+  Print "Installing NodeJS\t"
+  yum install nodejs make gcc-c++ -y &>>$LOG
+  Status_Check $?
+  ADD_APP_USER
+  DOWNLOAD
+  Print "Download NodeJS Dependencies"
+  cd /home/roboshop/${COMPONENT}
+  npm install --unsafe-perm &>>$LOG
+  Status_Check $?
+  chown roboshop:roboshop -R /home/roboshop
+  SystemdD_Setup
+}
+
