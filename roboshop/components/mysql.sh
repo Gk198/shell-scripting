bacf2300552a8b5be1ff0f746401a76790049a3d
Gk198 <gorakh.kumar@gmail.com>
2021-09-15 09:33:05 +0000

    mysql script

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -5,11 +5,13 @@
 source components/common.sh
 
 Print "Setup MySQL Repo\t\t"
+
  echo '[mysql57-community]
 name=MySQL 5.7 Community Server
 baseurl=http://repo.mysql.com/yum/mysql-5.7-community/el/7/$basearch/
 enabled=1
 gpgcheck=0' > /etc/yum.repos.d/mysql.repo
+
 SystemdD_Setup $?
 
 Print "Install MySQL Service"
