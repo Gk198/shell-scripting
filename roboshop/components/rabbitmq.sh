50daea9f44408cb73c33b731cfde6f6398c7e082
Gk198 <gorakh.kumar@gmail.com>
2021-09-16 10:08:42 +0000

    rabbitmq script

--- roboshop/components/rabbitmq.sh
+++ roboshop/components/rabbitmq.sh
@@ -5,15 +5,11 @@ source components/common.sh
 
 
 Print "Install ErLang\t"
- 
- yum list installed | grep erlang &>>/tmp/log
-  if [ $? -eq 0 ]; then
+  yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y &>>/tmp/log
   
-    echo "Package Already installed" &>>/tmp/log
-  else
-    yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y &>>/tmp/log
-  fi
-Status_Check $?
+ Status_Check $?
+ 
+ exit
 
 Print "Setup RabbitMQ Repos"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>>/tmp/log
