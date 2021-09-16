0b3e22adb6a82dcd855ab4a111f14e1a39f8ce5a
Gk198 <gorakh.kumar@gmail.com>
2021-09-16 05:58:35 +0000

    common update

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -31,7 +31,7 @@ if [ $SUI -ne 0 ]; then
 }
 
 DOWNLOAD() {
-  Print "Downloading ${COMPONENT} Content\t"
+  Print "Downloading ${COMPONENT} Content"
   curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/roboshop-devops-project/${COMPONENT}/archive/main.zip" &>>/tmp/log
   Status_Check $?
   Print "Extracting ${COMPONENT}\t"
